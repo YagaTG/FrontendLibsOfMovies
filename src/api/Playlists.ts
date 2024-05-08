76df590931b5813e21c654cafba373c46cb76c6c
Egor <egor.314@mail.ru>
2024-05-08 19:10:47 +0300

    add api for playlist

---
+++ src/api/Playlists.ts
@@ -0,0 +1,30 @@
+const ip = "192.168.0.101";
+
+export const createPlaylist = async (name, description, userId, playlist) => {
+  const playlistData = playlist.map((item) => {
+    return { id: item.id, name: item.value };
+  });
+  console.log(playlistData);
+  await fetch(`http://${ip}:3500/api/createPlaylist`, {
+    method: "post",
+    body: JSON.stringify({
+      name,
+      description,
+      userId,
+      playlist: JSON.stringify(playlistData),
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": "true",
+    },
+  })
+    .then((r) => r.json())
+    .then((data) => console.log(data))
+    .catch((err) => alert(err));
+};
+
+export const getUserPlaylist = (userId: number) => {
+  return fetch(`http://${ip}:3500/api/getUserPlaylists?userId=${userId}`).then(
+    (res) => res.json()
+  );
+};
