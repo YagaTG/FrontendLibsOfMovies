c0c3cd656cc603717d520192f9d3d3b327888532
Egor <egor.314@mail.ru>
2024-05-23 19:18:55 +0300

    add rating movies

--- src/api/Movie.ts
+++ src/api/Movie.ts
@@ -31,7 +31,26 @@ export const getMovie = (id: string | undefined, succesFunc) => {
   // return;
 };
 
-export const ratingMovie = (): void => {
+export const ratingMovie = (
+  userId: number,
+  movieId: number,
+  rating: number
+) => {
   //Отправка оценки фильма
-  return;
+  return fetch(`http://${ip}:3500/api/ratingMovie`, {
+    method: "post",
+    credentials: "include",
+    body: JSON.stringify({ userId, movieId, rating }),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": "true",
+    },
+  }).then((r) => r.json());
+};
+
+export const getMovieRating = (userId: string, movieId: string) => {
+  return fetch(
+    `http://${ip}:3500/api/getMovieRating?userId=${userId}&movieId=${movieId}`,
+    { credentials: "include" }
+  ).then((r) => r.json());
 };
