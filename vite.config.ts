343de09a4624eb7c6a0b6bc48c8a5b6e10dc150e
Egor <egor.314@mail.ru>
2024-05-28 00:30:42 +0300

    change ip

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://192.168.0.100:3500/",
-  //       changeOrigin: true,
-  //       cookiePathRewrite: { "*": "/" },
-  //     },
-  //   },
-  // },
   server: {
     host: '0.0.0.0',
   }
